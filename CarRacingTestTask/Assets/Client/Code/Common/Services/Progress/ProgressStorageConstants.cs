40611d1e22012ac06fb03324a373ae6ab18def02
Alenya <nevskial686@gmail.com>
2024-08-07 12:11:43 +0300

    Fix: saveLoader the same place to save the editor and the Windows progress

--- CarRacingTestTask/Assets/Client/Code/Common/Services/Progress/ProgressStorageConstants.cs
+++ CarRacingTestTask/Assets/Client/Code/Common/Services/Progress/ProgressStorageConstants.cs
@@ -7,9 +7,16 @@ namespace Client.Code.Common.Services.Progress
     public static class ProgressStorageConstants
     {
         private const string FilesExtension = "data";
-
-        public static readonly string DirectoryPath = Path.Combine(Application.persistentDataPath, "Saves");
-        public static readonly string FileName = Path.ChangeExtension(nameof(ProgressData), FilesExtension);
-        public static readonly string FilePath = Path.Combine(DirectoryPath, FileName);
+        private const string SaveFolderName = "Saves";
+        
+        private static readonly string _fileName = Path.ChangeExtension(nameof(ProgressData), FilesExtension);
+#if UNITY_EDITOR
+        private static readonly string _baseDirectoryPath = Directory.GetParent(Application.dataPath)?.FullName;
+#else
+        private static readonly string _baseDirectoryPath = Application.persistentDataPath;
+#endif
+        
+        public static readonly string DirectoryPath = Path.Combine(_baseDirectoryPath, SaveFolderName);
+        public static readonly string FilePath = Path.Combine(DirectoryPath, _fileName);
     }
 }
